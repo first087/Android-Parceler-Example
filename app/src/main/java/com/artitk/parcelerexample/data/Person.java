f6766d727d94b1ad82524e243bce4a0dcf36a8b1
Artit Kiuwilai <first087@gmail.com>
2015-06-04 14:13:26 +0700

    Fixed Parceler warning!

--- app/src/main/java/com/artitk/parcelerexample/data/Person.java
+++ app/src/main/java/com/artitk/parcelerexample/data/Person.java
@@ -5,9 +5,9 @@ import org.parceler.Parcel;
 @Parcel
 public class Person {
 
-    private String fullname;
-    private int gender;
-    private int age;
+    String fullname;
+    int gender;
+    int age;
 
     public Person() { /* Required empty bean constructor */ }
 
